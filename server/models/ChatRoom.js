bf5c0fd6ee86c1e31293a992c955d1237a9ae0d8
Ajit raut <ajitraut9561@gmail.com>
2024-12-03 20:22:19 +0530

    ChatRoom Model modified

--- server/models/ChatRoom.js
+++ server/models/ChatRoom.js
@@ -5,6 +5,7 @@ const ChatRoomSchema = new mongoose.Schema(
     name: { type: String },
     isGroup: { type: Boolean, default: false },
     members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    admin: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
   },
   { timestamps: true }
 );

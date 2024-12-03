ce6e0b8c15a6d289117ba77abc645e7b07ef84d6
Ajit raut <ajitraut9561@gmail.com>
2024-12-03 23:20:37 +0530

    User controller & Auth Route Added

---
+++ server/controllers/userController.js
@@ -0,0 +1,53 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    if (!username || !email || !password) {
+      res.status(404).json("Plaease Fill All Filds Mandatory");
+    }
+    const existingEmail = await User.findOne({ email });
+
+    if (existingEmail) {
+      res.status(400).json({ msg: "User Already Exist" });
+    }
+    const user = new User({
+      username,
+      email,
+      password,
+    });
+
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    res.json({ msg: "welcome", userdata, token });
+  } catch (error) {
+    res.status(500).json({ msg: "Server Error" });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { register, login };
--- server/server.js
+++ server/server.js
@@ -1,18 +1,44 @@
 const express = require("express");
 const http = require("http");
-const socketio = require('socket.io');
+const socketio = require("socket.io");
+const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config();
+const autRoute = require("./routes/authRoute")
 
 const app = express();
-const server = http.createServer(app)
-const io = socketio(server)
- 
+app.use(cors());
+app.use(express.json())
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
 connectDB();
 
+app.use("/user" ,autRoute)
 
-PORT = 3000 || process.env.PORT;
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
 
-server.listen(PORT , ()=>{
-    console.log(`server runnning on ${PORT}`)
-})
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+  });
+
+  socket.on("send-message", (data) => {
+    io.to(data.room).emit("receive-message", data.message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+PORT = 5000 || process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`server runnning on ${PORT}`);
+});

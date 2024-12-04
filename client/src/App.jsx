0a43686137e94c5c96b74facd02e298c3c6be58e
Ajit raut <ajitraut9561@gmail.com>
2024-12-04 23:06:16 +0530

    Some Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
 
-const App = () => {
-  return (
-    <>
-      
-    </>
-  )
+const AppRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/login" />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      // {
+      //   path: "home",
+      //   element: <Home />,
+      // },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={AppRouter} />;
 }
 
-export default App
+export default App;
